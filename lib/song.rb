f03a79a3cf178ada50503ff949b09648529dd6d0
srappaport <sarah.rappaport@greendot.org>
2018-05-19 19:15:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,13 @@ class Song
   attr_accessor :name
   attr_accessor :artist
   attr_accessor :genre
-
+  @@count = 0 
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count +=1
   end
 
   def new
